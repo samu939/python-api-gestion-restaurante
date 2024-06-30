cf1068dc9c911f41fcbb208661ba1f111626b128
Luis Gil <giluis2000@gmail.com>
2024-06-30 18:08:17 -0400

    consultar menu listo

--- apps/menus/infraestructure/responses/menus_responses.py
+++ apps/menus/infraestructure/responses/menus_responses.py
@@ -1,6 +1,15 @@
 from uuid import UUID
 from core.infrastructure.utils.schemas_base import BaseSchema
 
+class MenuPlates(BaseSchema):
+    id: UUID
+    name: str
+    price: float
+
+class GetMenuWithPlatesResponse(BaseSchema):
+    id: UUID
+    name: str
+    plates: list[MenuPlates]
 
 class GetMenuResponse(BaseSchema):
     id: UUID
--- apps/menus/infraestructure/routes/menus_routes.py
+++ apps/menus/infraestructure/routes/menus_routes.py
@@ -1,11 +1,16 @@
+from uuid import UUID
 from databases import Database
 from fastapi import APIRouter, Depends
 
 from apps.auth.infraestructure.dependecies.auth_dependecies import get_current_active_user
 from apps.menus.application.services.get_all_menus_application_service import GetAllMenusApplicationService
+from apps.menus.application.services.get_menu_application_service import GetMenuByIdApplicationService
+from apps.menus.domain.value_objects.menu_id import MenuId
 from apps.menus.infraestructure.mappers.menus_mapper import MenuMapper
-from apps.menus.infraestructure.repositories.db_menus_repository import DbPMenusRepository
-from apps.menus.infraestructure.responses.menus_responses import GetAllMenusResponse, GetMenuResponse
+from apps.menus.infraestructure.repositories.db_menus_repository import DbMenusRepository
+from apps.menus.infraestructure.responses.menus_responses import GetAllMenusResponse, GetMenuResponse, GetMenuWithPlatesResponse
+from apps.plates.infrastructure.mappers.plates_mapper import PlateMapper
+from apps.plates.infrastructure.repositories.db_plates_repository import DbPlatesRepository
 from apps.user.infrastructure.db_entity.user_in_db import UserInDB
 from core.application.decorators.exception_decorator import ExceptionDecorator
 from db.db_dependencies import get_database
@@ -23,7 +28,7 @@ async def getMenus(
     current_user: UserInDB = Depends(get_current_active_user)
 ):
     
-    service = ExceptionDecorator(GetAllMenusApplicationService(menus_repository= DbPMenusRepository(db, MenuMapper())))
+    service = ExceptionDecorator(GetAllMenusApplicationService(menus_repository= DbMenusRepository(db, MenuMapper())))
     response = (await service.execute(None)).unwrap()
     menus: list[GetMenuResponse] = []
 
@@ -34,3 +39,13 @@ async def getMenus(
         })
     
     return GetAllMenusResponse(menus=menus)
+
+@menus_router.get("get/{id}", response_model=GetMenuWithPlatesResponse, name="menu:getById")
+async def getPlateById(
+    id: UUID,
+    db: Database = Depends(get_database),
+    current_user: UserInDB = Depends(get_current_active_user)
+):
+    service = ExceptionDecorator(GetMenuByIdApplicationService(menus_repository=DbMenusRepository(db, MenuMapper()), plates_repository=DbPlatesRepository(db, PlateMapper())))
+    response = (await service.execute(MenuId(id))).unwrap()
+    return response
