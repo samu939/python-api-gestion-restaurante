3bc9c1f73b5ad24c652cf22afcce80f85e186168
Luis Gil <giluis2000@gmail.com>
2024-07-01 14:44:24 -0400

    modificar platos calidad

---
+++ apps/plates/application/services/modify_plate_application_service.py
@@ -0,0 +1,41 @@
+from typing import Awaitable
+from apps.ingredients.domain.value_objects.ingredient_id import IngredientId
+from apps.plates.application.errors.plate_not_found import PlateNotFoundApplicatonError
+from apps.plates.domain.plate import Plate
+from apps.plates.domain.repositories.plates_repository import PlateRepository
+from apps.plates.domain.value_objects.ingredient_for_plate_quantity import IngredientForPlateQuantity
+from apps.plates.domain.value_objects.plate_id import PlateId
+from apps.plates.domain.value_objects.plate_ingredient import PlateIngredient
+from apps.plates.domain.value_objects.plate_price import PlatePrice
+from apps.plates.infrastructure.dtos.modify_plate_dto import ModifyPlateDto
+from core.application.events.event_handler import EventHandler
+from core.application.results.result_wrapper import Result
+from core.application.services.application_service import ApplicationService
+
+
+class ModifyplateApplicationService(ApplicationService[ModifyPlateDto, str]):
+    def __init__(self, plates_repository: PlateRepository, event_handler: EventHandler) -> None:
+        self.plates_repository = plates_repository
+        self.event_handler = event_handler
+
+    async def execute(self, input: ModifyPlateDto) -> Awaitable[Result[str]]:
+
+        plate = await self.plates_repository.get_plate_by_id(PlateId(input.id))
+        if plate is None:
+            return Result[str].failure(
+                error=PlateNotFoundApplicatonError(PlateId(input.id))
+            )
+
+        domain_plate_ingredients = []
+
+        for ingredient in input.ingredients:
+            domain_plate_ingredients.append(PlateIngredient({
+                'ingredient_id': IngredientId(ingredient.id),
+                'quantity': IngredientForPlateQuantity(ingredient.quantity)
+            }))
+
+        modified_plate = Plate(plate.id, plate.name, plate.description,
+                             PlatePrice(input.price), domain_plate_ingredients)
+        await self.event_handler.publish_events(modified_plate.pull_events())
+        await self.plates_repository.update(modified_plate)
+        return Result[str].success(value="Plato guardado")
--- apps/plates/infrastructure/queries/plates_queries.py
+++ apps/plates/infrastructure/queries/plates_queries.py
@@ -1,31 +1,42 @@
 GET_ALL_PLATES = """
     SELECT
-            plate.id,
-            plate.name,
-            plate.description,
-            plate.price,
-            json_agg(json_build_object('ingredient_id', ingredient_plate.fk_ingredient, 'quantity', ingredient_plate.quantity)) AS ingredients
-        FROM
-            plate
-        JOIN
-            ingredient_plate ON plate.id = ingredient_plate.fk_plate
-        GROUP BY
-            plate.id
+    plate.id,
+    plate.name,
+    plate.description,
+    plate.price,
+    COALESCE(json_agg(
+        json_build_object('ingredient_id', ingredient.id, 'ingredient_name', ingredient.name, 'quantity', ingredient_plate.quantity)
+    ) FILTER (WHERE ingredient.id IS NOT NULL), '[]') AS ingredients
+    FROM
+        plate
+    LEFT JOIN
+        ingredient_plate ON ingredient_plate.fk_plate = plate.id
+    LEFT JOIN
+        ingredient ON ingredient_plate.fk_ingredient = ingredient.id
+    GROUP BY
+        plate.id
 """
 
 GET_PLATE_BY_ID = """
     SELECT
-            plate.id,
-            plate.name,
-            plate.description,
-            plate.price,
-            json_agg(json_build_object('ingredient_id', ingredient_plate.fk_ingredient, 'ingredient_name', ingredient.name, 'quantity', ingredient_plate.quantity)) AS ingredients
-        FROM plate, ingredient_plate, ingredient
-        WHERE ingredient_plate.fk_ingredient = ingredient.id
-		AND ingredient_plate.fk_plate = plate.id
-		AND plate.id = :id
-        GROUP BY
-            plate.id
+    plate.id,
+    plate.name,
+    plate.description,
+    plate.price,
+    COALESCE(json_agg(
+        json_build_object('ingredient_id', ingredient.id, 'ingredient_name', ingredient.name, 'quantity', ingredient_plate.quantity)
+    ) FILTER (WHERE ingredient.id IS NOT NULL), '[]') AS ingredients
+    FROM
+        plate
+    LEFT JOIN
+        ingredient_plate ON ingredient_plate.fk_plate = plate.id
+    LEFT JOIN
+        ingredient ON ingredient_plate.fk_ingredient = ingredient.id
+    WHERE
+        plate.id = :id
+    GROUP BY
+        plate.id
+
 """
 
 INSERT_NEW_PLATE = """
@@ -33,4 +44,15 @@ INSERT_NEW_PLATE = """
 """
 INSERT_NEW_PLATE_INGREDIENTS = """
     INSERT INTO ingredient_plate(quantity, fk_plate, fk_ingredient) values(:quantity, :plate_id, :ingredient_id);
+"""
+
+UPDATE_PLATE = """
+    UPDATE plate
+    SET price = :price
+    WHERE id = :id
+"""
+
+DELETE_PLATE_INGREDIENTS = """
+    DELETE FROM ingredient_plate
+    WHERE fk_plate = :id;
 """
